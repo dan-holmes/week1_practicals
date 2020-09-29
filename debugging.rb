79d3a6e8e0c87bd64900b09da4bf9a9d4f7f3b50
Dan Holmes <dan@dans-air.home>
2020-09-29 12:06:01 +0100

    completed the debugging practical

--- debugging.rb
+++ debugging.rb
@@ -21,3 +21,43 @@ def decode(ciphertext, key)
   end
   plaintext_chars.join
 end
+
+# Interlude: getting visibility #
+
+def factorial(n)
+  product = 1
+  p "at the start product is #{product}"
+  while n > 0
+    p "we multiply #{product} by #{n}"
+    product *= n
+    p "we get #{product}"
+    n -= 1
+  end
+  product
+end
+
+
+# 4. #
+
+def encode(plaintext, key)
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
+# When creating your alphabet array make sure to use '..' to create
+# the range. '...' doesn't include the first and last letters
+  ciphertext_chars = plaintext.chars.map do |char|
+    (65 + cipher.find_index(char)).chr
+  end
+  ciphertext_chars.join
+end
+
+def decode(ciphertext, key)
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
+  plaintext_chars = ciphertext.chars.map do |char|
+    cipher[char.ord - 65] # You must take 65 away from the ord of the char.
+                          # This removes the first 65 encoding characters which
+                          # are mainly punctuation, and starts it at the letter 'a'
+  end
+  plaintext_chars.join
+end
+
+# puts encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+# puts decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
